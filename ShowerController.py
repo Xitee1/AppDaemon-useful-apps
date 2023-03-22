3aa1e447f8b0bfa5fb3cbd0cd2329187fb7c7e80
Xitee <mato.bartusch123@gmail.com>
2023-03-22 17:19:45 +0100

    Refactor methods to lowercase

--- ShowerController.py
+++ ShowerController.py
@@ -2,6 +2,8 @@ import hassapi as hass
 from enum import Enum
 import asyncio
 
+testmode = True
+
 LISTEN_BIN_SENSOR_1         = "binary_sensor.some_switch"
 
 WATER_HEATER_SWITCH         = "switch.warmwasserpumpe"
@@ -10,6 +12,7 @@ LIGHT_EFFECT_WATER_WARMING  = "blinkred"
 LIGHT_EFFECT_WATER_WARM     = "green_effect"
 LIGHT_PLAYLIST_SHOWERING    = "duschen"
 LIGHT_EFFECT_SHOWERING_LONG = "langduschen"
+
 
 class ButtonAction(Enum):
     SHORT = 1
@@ -32,17 +35,17 @@ class ShowerController(hass.Hass):
     def initialize(self):
         print("Initializing ShowerController")
 
-        self.listen_state(self.handleButtonPress(), LISTEN_BIN_SENSOR_1, new="on")
+        self.listen_state(self.handle_button_press(), LISTEN_BIN_SENSOR_1, new="on")
         # TODO execute button press
 
-    def handleButtonPress(self, action=ButtonAction.SHORT, kwargs=None):
+    def handle_button_press(self, action=ButtonAction.SHORT, kwargs=None):
         if action == ButtonAction.SHORT:
             # Execute next-action
             print("Shower-Button pressed shortly")
 
-            self.nextState()
+            self.next_state()
 
-            self.executeActions()
+            self.execute_actions()
 
         if action == ButtonAction.LONG:
             # Cancel script
@@ -50,17 +53,17 @@ class ShowerController(hass.Hass):
 
             self.currentState = State.IDLE
 
-            self.executeActions()
+            self.execute_actions()
 
-    def nextState(self):
+    def next_state(self):
         if self.currentState == len(State):
             self.currentState = 1
         else:
             self.currentState += 1
 
     # Execute action based on state
-    def executeActions(self):
-        self.cancelTimeout()
+    def execute_actions(self):
+        self.cancel_timeout()
 
         if self.currentState == State.IDLE:
             self.turn_on(LIGHT_DEFAULT_MODE_SCRIPT)
@@ -69,26 +72,26 @@ class ShowerController(hass.Hass):
             self.turn_on(LIGHT_EFFECT_WATER_WARMING)
             self.turn_on(WATER_HEATER_SWITCH)
             # Wait x minutes to next state
-            self.timeout_task = asyncio.create_task(self.setTimeout(10))
+            self.timeout_task = asyncio.create_task(self.set_timeout(10))
 
         if self.currentState == State.WATER_WARM:
             self.turn_on(LIGHT_EFFECT_WATER_WARM)
-            self.setTimeout(20)
+            self.set_timeout(20)
 
         if self.currentState == State.SHOWERING:
             self.turn_on(LIGHT_PLAYLIST_SHOWERING)
-            self.setTimeout(15)
+            self.set_timeout(15)
 
         if self.currentState == State.SHOWERING_LONG:
             self.turn_on(LIGHT_EFFECT_SHOWERING_LONG)
-            self.setTimeout(15)
+            self.set_timeout(15)
 
-    def cancelTimeout(self):
+    def cancel_timeout(self):
         # TODO cancel timer
         self.timeout_task.cancel()
         print("TODO cancel timer")
 
-    async def setTimeout(self, minutes):
+    async def set_timeout(self, minutes):
         await asyncio.wait(60*minutes)
-        self.nextState()
-        self.executeActions()
+        self.next_state()
+        self.execute_actions()
