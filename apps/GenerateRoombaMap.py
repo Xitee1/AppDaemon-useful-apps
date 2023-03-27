b98ff7ceb1d2365adfea43a2246ac62ca150fb14
Xitee <mato.bartusch123@gmail.com>
2023-03-27 15:40:40 +0200

    Update GenerateRoombaMap.py

--- apps/GenerateRoombaMap.py
+++ apps/GenerateRoombaMap.py
@@ -16,15 +16,15 @@ POINT_COLOR_DOCKED = (255, 255, 255)
 # TODO need to use absolute path? Relative paths in this format do not work. fonts folder is in the apps folder, where the script is
 font = ImageFont.truetype("/config/appdaemon/apps/fonts/Arimo-Bold.ttf", size=32)
 
-VACUUM_LOG_PATH = "/config/www/tmp/vacuum_{name}/vacuum.log"
-MAP_OUTPUT_PATH = "/config/www/tmp/vacuum_{name}/map.png"
+VACUUM_LOG_PATH = "{tmp_path}/vacuum_{name}/vacuum.log"
+MAP_OUTPUT_PATH = "{tmp_path}/vacuum_{name}/map.png"
 
 
 class GenerateImage(hass.Hass):
     """
     Generate a map where roomba has cleaned your home.
 
-    This also requires a little extra setup in HA:
+    To have the image in a camera entity:
     - Add the following to your configuration.yaml:
         camera:
           - platform: local_file
@@ -43,6 +43,8 @@ class GenerateImage(hass.Hass):
         Your vacuum entity. E.g. 'vacuum.roomba'
     - vacuum_name:
         Used for naming the vacuum folders.
+    - tmp_path:
+        Path to a tmp folder where the log and image should be stored. E.g. /config/www/tmp
     - floor_plan_location:
         The file path of your floor plan. E.g. '/config/floorplans/home.png'
     - offset_cords_x:
@@ -52,17 +54,19 @@ class GenerateImage(hass.Hass):
     - image_rotation:
         If the cords aren't drawn correctly to the map, you can try it with rotating the image (0, 90, 180, 270)
 
-    FIXME This requires some extra steps in HA.
-    TODO log roomba's cords with AD and do not require additional setup in HA.
-
     """
 
     def initialize(self):
+        # Init internal vars
         self.line_space = 0
         self.draw = None
+
+        self.vacuum_cords = []
         self.draw_last_x = 0
         self.draw_last_y = 0
+        self.write_log_to_file = True
 
+        # Init args
         self.offset_cords_x = int(self.args['offset_cords_x'])
         self.offset_cords_y = int(self.args['offset_cords_y'])
 
@@ -71,77 +75,98 @@ class GenerateImage(hass.Hass):
         self.vacuum = self.get_entity(self.args['vacuum_entity'])
 
         self.map_path = self.args['floor_plan_location']
-        self.vacuum_log_path = VACUUM_LOG_PATH.replace("{name}", self.args['vacuum_name'])
-        self.map_output_path = MAP_OUTPUT_PATH.replace("{name}", self.args['vacuum_name'])
+        self.vacuum_log_path = VACUUM_LOG_PATH.replace("{tmp_path}", self.args['tmp_path']).replace("{name}", self.args['vacuum_name'])
+        self.map_output_path = MAP_OUTPUT_PATH.replace("{tmp_path}", self.args['tmp_path']).replace("{name}", self.args['vacuum_name'])
 
         self.image_rotation = self.args['image_rotation']
 
+        # Prepare app
+        self.load_log()
         self.generate_image()
 
-        # Listen for position changes
+        # Listeners
         # TODO reduce disk writes
         self.run_every(self.generate_image, start=self.datetime(), interval=5)
-        #self.vacuum.listen_state(self.generate_image, attribute="all")
         self.vacuum.listen_state(self.write_log, attribute="position")
         self.vacuum.listen_state(self.clear_log, old="docked", new="cleaning", duration=10)
+        self.vacuum.listen_state(self.save_log, new="docked")
 
     def mylog(self, msg):
         if self.debug:
             self.log(msg)
+    """
+    Log UTILS
+    """
+    def load_log(self):
+        """
+        Load the log from the log file
+        """
+        with open(self.vacuum_log_path, 'r') as f:
+            reader = csv.reader(f, delimiter=",")
 
+            for row in reader:
+                x = int(row[0])
+                y = int(row[1])
+                self.vacuum_cords.append([x, y])
+
+    def save_log(self):
+        """
+        Save the log to the log file
+        """
+        with open(self.vacuum_log_path, "r+") as f:
+            f.truncate(0)
+
+        with open(self.vacuum_log_path, "a") as f:
+            for c in self.vacuum_cords:
+                f.write(f"{c[0]},{c[1]}\n")
+
+    def write_log(self, entity, attribute, old, new, kwargs):
+        """
+        Add current cords to log. This however does not save the log to the file.
+        """
+        vacuum_position = new.strip("()").split(', ')
+        x = int(vacuum_position[0])
+        y = int(vacuum_position[1])
+
+        self.vacuum_cords.append([x, y])
+
+        # Save log after every write for debug
+        if self.debug:
+            with open(self.vacuum_log_path, "a") as f:
+                f.write(f"{x},{y}\n")
+
+    def clear_log(self):
+        """
+        Clears the cords array and the file
+        """
+        self.vacuum_cords = []
+        with open(self.vacuum_log_path, 'r+') as f:
+            f.truncate(0)
+
+    """
+    Image tools
+    """
     def write_point(self, color, location):
         # cv2.circle(image, location, radius=14, color=color, thickness=-1)
-        self.mylog("TODO draw point")
+        # TODO draw point
+        i = 0 # TODO remove (prevents method empty error)
 
     def write_point_info(self, color, text):
         self.write_point(color, (15, self.line_space + 15))
         self.draw.text((30, self.line_space + 23), text, color, font=font)
         self.line_space += 30
 
-    def write_log(self, entity, attribute, old, new, kwargs):
-        f = open(self.vacuum_log_path, "a")
-
-        #vacuum_position = self.vacuum.get_state(attribute="position").strip("()").split(', ')
-
-        vacuum_position = new.strip("()").split(', ')
-        x = vacuum_position[0]
-        y = vacuum_position[1]
-
-        f.write(f"{x},{y}\n")
-        f.close()
-
-        #self.update_image(int(x), int(y))
-
-    def clear_log(self):
-        with open(self.vacuum_log_path, 'r+') as file:
-            file.truncate(0)
-
-    def update_image(self, x, y):
-        # TODO not really working because the old roomba location point is still on the map.
-        # Pillow does not have an undo function for that, which means we have to render the whole image again.
-
-        image = Image.open(self.map_path)
-        image = image.rotate(self.image_rotation)
-        self.draw = ImageDraw.Draw(image)
-
-        x += self.offset_cords_x
-        y += self.offset_cords_y
-
-        self.draw.line([(y, x), (self.draw_last_y, self.draw_last_x)], width=LINE_THICKNESS, fill=LINE_COLOR_RGB)
-        self.draw_last_x = x
-        self.draw_last_y = y
-
-        image = image.rotate(-self.image_rotation)
-        image.save(self.map_output_path, "PNG")
-
     def generate_image(self, entity=None, attribute=None, old=None, new=None, kwargs=None):
+        """
+        Generates the image and saves it to disk.
+        """
         # Get data from HA
         vacuum_state = self.vacuum.get_state()
 
         if vacuum_state == "docked":
-            return;
+            return
 
-        self.mylog("Vacuum state: {}".format(vacuum_state))
+        #self.mylog("Vacuum state: {}".format(vacuum_state))
 
         # Load base image
         image = Image.open(self.map_path)
@@ -156,28 +181,18 @@ class GenerateImage(hass.Hass):
         image = image.rotate(self.image_rotation)
         self.draw = ImageDraw.Draw(image)
 
-        with open(self.vacuum_log_path, 'r') as f:
-            reader = csv.reader(f, delimiter=",")
+        first_run = True
+        for c in self.vacuum_cords:
+            x = c[0] + self.offset_cords_x
+            y = c[1] + self.offset_cords_y
 
-            #next(reader, None)
+            if first_run:
+                first_run = False
+            else:
+                self.draw.line([(y, x), (self.draw_last_y, self.draw_last_x)], width=LINE_THICKNESS, fill=LINE_COLOR_RGB)
 
-            first_line = True
-
-            for row in reader:
-                x = int(row[0]) + self.offset_cords_x
-                y = int(row[1]) + self.offset_cords_y
-                # self.mylog("X:{} Y:{}".format(x, y))
-                # self.mylog("Last X:{} Last Y:{}".format(self.draw_last_x, self.draw_last_y))
-
-                if first_line:
-                    self.draw_last_x = x
-                    self.draw_last_y = y
-                    first_line = False
-
-                else:
-                    self.draw.line([(y, x), (self.draw_last_y, self.draw_last_x)], width=LINE_THICKNESS, fill=LINE_COLOR_RGB)
-                    self.draw_last_x = x
-                    self.draw_last_y = y
+            self.draw_last_x = x
+            self.draw_last_y = y
 
         # Write current vacuum state to the map image
         if vacuum_state == "docked":  # or vacuum_status == "charging"
