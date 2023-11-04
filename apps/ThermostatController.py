040dcaa31ee9d04cbd51d9fd47913fe639039679
Xitee <59659167+Xitee1@users.noreply.github.com>
2023-11-04 14:53:55 +0100

    Make ThermostatController.py working

--- apps/ThermostatController.py
+++ apps/ThermostatController.py
@@ -1,7 +1,5 @@
 import hassapi as hass
-from appdaemon.exceptions import TimeOutException
-import datetime as datetime
-
+import datetime
 
 class ThermostatController(hass.Hass):
     """
@@ -18,7 +16,7 @@ class ThermostatController(hass.Hass):
         # Init
         self.log("Initializing ThermostatController..")
         self.last_current_temp = 0
-        self.last_execution_time = datetime.now()
+        self.last_execution_time = datetime.datetime.now()
         self.last_valve_position = None
 
         # Config
@@ -26,62 +24,76 @@ class ThermostatController(hass.Hass):
         self.max_update_interval = 120 # Max update interval in seconds
 
         # Arguments
+        self.entity_enabled = self.get_entity(self.args['enabled']) if 'enabled' in self.args else None
         self.entity_thermostat = self.get_entity(self.args['entity_thermostat'])
         self.entity_valve_position = self.get_entity(self.args['entity_valve_position'])
 
         # App
         self.entity_thermostat.listen_state(self.update_valve_position)
-        self.run_every(self.update_valve_position, start="now+2", interval=self.max_update_interval)
+        self.entity_thermostat.listen_state(self.update_valve_position, attribute="temperature")
+        self.entity_thermostat.listen_state(self.update_valve_position, attribute="current_temperature")
         self.log("ThermostatController initialized!")
 
-    def log(self, msg):
-        if self.debug:
-            self.log(msg)
 
+    def add_execution_credit(self, kwargs=None):
+        if self.execution_credits < self.max_execution_credits:
+            self.execution_credits += 1
 
-    def update_valve_position(self, entity=None, attribute=None, old=None, new=None, kwargs=None):
-        time_difference = datetime.now() - self.last_execution_time
-        if time_difference.total_seconds() >= self.max_update_interval:
-            self.log("Not updating valve position (updated less than 2 minutes ago)")
+    def update_valve_position(self, attribute, entity=None, old=None, new=None, kwargs=None):
+        # TODO check if everything is available
+
+        # Check if controller is enabled
+        if self.entity_enabled is not None and self.entity_enabled.is_state("off"):
+            self.log("Controller is disabled.")
             return
 
-
+        # Init values
         current_temp = self.entity_thermostat.get_state(attribute="current_temperature")
         target_temp = self.entity_thermostat.get_state(attribute="temperature")
         difference_last_temp = abs(current_temp - self.last_current_temp)
 
-        if difference_last_temp >= self.min_temp_difference:
-            valve_position = self.get_valve_position(target_temp, current_temp)
+        # TODO bypass wait if attribute is not 'current_temperature'
+        # Check if temperature difference is too small (only if updated by current temp change)
+        if attribute == "current_temperature":
+            if difference_last_temp < self.min_temp_difference and current_temp < target_temp:
+                self.log("Not updating valve position (temperature difference too small)")
+                return
 
-            if valve_position != self.last_valve_position:
-                self.entity_valve_position.set_state(state=valve_position)
-                self.log(f"New valve position: {valve_position}")
-            else:
-                self.log("Not updating valve position (already same value)")
-
+        valve_position = self.get_valve_position(target_temp, current_temp)
+        if valve_position != self.last_valve_position:
+            self.entity_valve_position.set_state(state=valve_position)
+            self.last_valve_position = valve_position
+            self.log(f"New valve position: {valve_position}")
         else:
-            self.log("Not updating valve position (temperature difference too small)")
+            self.log(f"Not updating valve position (already same value ({valve_position}))")
 
         self.last_current_temp = current_temp
 
 
     def get_valve_position(self, target_temp, current_temp):
         difference = target_temp - current_temp # How many degrees need to be heated
+        self.log(f"Valve position calculation: Temp difference: {difference}")
 
-        # TODO Make this definable per app instance:
-        if difference <= 0.1:
+        # TODO Make this definable per app instance
+        if difference <= 0.0:
             return 0
 
-        if difference >= 0.2:
-            return 55
+        if difference <= 0.1:
+            return 20
 
-        if difference >= 0.5:
-            return 130
+        if difference <= 0.2:
+            return 50
 
-        if difference >= 1.0:
-            return 190
+        if difference <= 0.5:
+            return 80
 
-        if difference >= 1.5:
+        if difference <= 0.7:
+            return 120
+
+        if difference <= 1.0:
+            return 160
+
+        if difference <= 1.5:
             return 210
 
         return 255
