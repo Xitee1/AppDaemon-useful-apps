b9b9fc0a1685f51fa5d7834aafcd95fd3446dc02
Xitee <59659167+Xitee1@users.noreply.github.com>
2024-01-19 15:11:06 +0100

    Fix severe bugs to make app working

--- apps/SolarDeviceController.py
+++ apps/SolarDeviceController.py
@@ -16,28 +16,27 @@ class BatteryState:
         self.end = end
         self.min_charge_power = min_charge_power
 
+
 class Device:
-    def __init__(self, entity_id, consumption, enabled=False, enabled_by=None, min_cycle_duration=30):
+    def __init__(self, ad, entity_id, consumption, enabled=False, enabled_by=None, min_cycle_duration=30):
         self.entity_id = entity_id
         self.consumption = consumption
         self.__enabled = enabled
         self.__enabled_by_id = enabled_by
         self.__min_cycle_duration = min_cycle_duration
 
-        self.__entity = self.get_entity(self.entity_id)
-        self.__enabled_by = self.get_entity(self.__enabled_by) if self.__enabled_by_id else None
+        self.__ad = ad
+        self.__entity = ad.get_entity(self.entity_id)
+        self.__enabled_by = ad.get_entity(self.__enabled_by) if self.__enabled_by_id else None
+
+    def is_wanted_state(self, state="on"):
+        return self.__entity.get_state() == state
 
     def turn_on(self):
-        if self.__entity.get_state() == 'off':
-            self.__entity.turn_on()
-            return True
-        return False
+        self.__entity.turn_on()
 
     def turn_off(self):
-        if self.__entity.get_state() == 'on':
-            self.__entity.turn_off()
-            return True
-        return False
+        self.__entity.turn_off()
 
     def is_enabled(self):
         if not self.__enabled:
@@ -49,34 +48,32 @@ class Device:
         return True
 
     def passed_min_toggle_interval(self):
-        last_changed_seconds = int(datetime.now().timestamp() -
-                                   self.convert_utc(self.__entity.get_state(attribute="last_changed")).timestamp())
-        return last_changed_seconds > device.min_cycle_duration
+        last_changed_seconds = int(
+            datetime.now().timestamp() -
+            self.__ad.convert_utc(self.__entity.get_state(attribute="last_changed")).timestamp()
+        )
+        return last_changed_seconds > self.__min_cycle_duration
 
     def is_powered_on(self):
         return self.__entity.get_state() == 'on'
-
 
 
 class SolarDeviceController(hass.Hass):
     """
     Controls devices based on solar production.
 
-    TODO Include battery percentage in calculations.
-
     """
     def initialize(self):
         self.log("Initializing SolarDeviceController")
 
         self.current_excess_state_timer = 0
-        self.last_excess_state = LastExcessState.NONE
+        self.last_excess_state = ExcessState.NONE
 
-        self.max_excess_state_timer = 60*5 # Stop counting if it has reached 5min
         self.excess_state_timer_add_factor = 1
         self.excess_state_timer_remove_factor = 5
 
         # Init values from params
-        self.debug = self.args['debug']
+        self.debug = bool(self.args['debug']) if 'debug' in self.args else False
 
         self.production_sensor = self.get_entity(self.args['production_sensor'])
         self.consumption_sensor = self.get_entity(self.args['consumption_sensor'])
@@ -85,12 +82,15 @@ class SolarDeviceController(hass.Hass):
         self.enabling_battery_percentage = int(self.args['enabling_battery_percentage']) if 'enabling_battery_percentage' in self.args else 0
         self.update_interval = int(self.args['update_interval']) if 'update_interval' in self.args else 10
 
+        self.max_excess_state_timer = (60 * 5) / self.update_interval  # Stop counting if it has been reached 5min
+
         # Initialize devices
         self.devices = []
         raw_devices = self.args['devices']
         for device in raw_devices:
             self.devices.append(
                 Device(
+                    ad=self,
                     entity_id=device['entity'],
                     consumption=int(device['consumption']),
                     enabled=device['enabled'] if 'enabled' in device else True,
@@ -114,9 +114,13 @@ class SolarDeviceController(hass.Hass):
                 )
 
         # Start loop
-        self.run_every(self.loop, start=f"now+3", interval=1)
+        self.run_every(self.loop, start=f"now+3", interval=self.update_interval)
 
         self.log("Initialization done!")
+
+    def clog(self, msg):
+        if self.debug:
+            self.log(msg)
 
     """
     The loops needs to be called exactly every second in order to work correctly.
@@ -135,8 +139,10 @@ class SolarDeviceController(hass.Hass):
                 for battery_state in self.battery_states:
                     # TODO check time ranges
                     in_time_range = True
-                    if battery_state.percentage <= battery_percentage and in_time_range:
+                    if battery_percentage <= battery_state.percentage and in_time_range:
                         excess_power -= battery_state.min_charge_power
+                        self.clog(f"Preserving {battery_state.min_charge_power}W excess power for the battery"
+                                  f"(Current percentage: {battery_percentage}).")
                         break
 
         # Add consumption of already powered on devices to access (to prevent toggling on each update)
@@ -153,7 +159,8 @@ class SolarDeviceController(hass.Hass):
             excess_state = self.last_excess_state
 
         if self.last_excess_state == excess_state:
-            self.current_excess_state_timer += self.excess_state_timer_add_factor
+            if self.current_excess_state_timer >= self.max_excess_state_timer:
+                self.current_excess_state_timer += self.excess_state_timer_add_factor
         else:
             # Add some buffer. For example afternoons the production is since 100 seconds 150W below the consumption
             # while it is cloudy. But if the sun passes through a cloud for like 5 seconds and after that the production
@@ -162,21 +169,21 @@ class SolarDeviceController(hass.Hass):
             # remove that value from it. This means the timer gets reduced a lot and will be at 0 after around 10 secs
             # but will not instantly set to 0.
             remove_amount = int((self.current_excess_state_timer / self.excess_state_timer_remove_factor))
-            if remove_amount < factor:
+            if remove_amount < self.excess_state_timer_remove_factor:
                 remove_amount = 0
             self.current_excess_state_timer -= remove_amount
 
         if self.current_excess_state_timer == 0:
             self.last_excess_state = excess_state
 
-        self.log(f"Excess power: {excess_power}; current_state_timer: {self.current_excess_state_timer}; excess_state: {self.last_excess_state}")
+        self.clog(f"Excess power: {excess_power}; State timer: {self.current_excess_state_timer}; Current state: {excess_state}; Last state: {self.last_excess_state}")
 
         # Control devices
         if self.last_excess_state == excess_state:
             for device in self.devices:
                 excess_power -= self.control_device(device, excess_power)
 
-            self.log(f"Excess: {excess_power}")
+            self.clog(f"Excess (controlled devices included): {excess_power}")
 
     """
     Turns a device on or off based on the excess power.
@@ -187,15 +194,28 @@ class SolarDeviceController(hass.Hass):
         if not device.is_enabled():
             return 0
 
-        if not device.passed_min_toggle_interval():
-            self.log(f"Did not toggle device {device.entity_id} because of minimum toggle interval.")
-            return 0
+        passed_min_toggle_interval = device.passed_min_toggle_interval()
 
         if device.consumption < excess_power:
-            if device.turn_on():
-                self.log(f"Turned on device {device.entity_id}.")
-            return device.consumption
+            if device.is_wanted_state("on"):
+                return device.consumption
+            else:
+                if passed_min_toggle_interval:
+                    device.turn_on()
+                    self.clog(f"Turned on device {device.entity_id}.")
+                    return device.consumption
+                else:
+                    self.clog(f"Did not turn on device {device.entity_id} because of minimum toggle interval.")
+                    return 0
+
         else:
-            if device.turn_off():
-                self.log(f"Turned off device {device.entity_id}.")
-            return 0
+            if device.is_wanted_state("off"):
+                return 0
+            else:
+                if passed_min_toggle_interval:
+                    device.turn_off()
+                    self.clog(f"Turned off device {device.entity_id}.")
+                    return 0
+                else:
+                    self.clog(f"Did not turn off device {device.entity_id} because of minimum toggle interval.")
+                    return device.consumption
